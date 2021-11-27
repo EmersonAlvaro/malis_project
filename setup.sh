636fb2dfe2f5980714c28339a02fd94aee2417c8
Emerson Cardoso <cardoso@eurecom.fr>
2021-11-27 20:13:59 +0100

    genesis

---
+++ setup.sh
@@ -0,0 +1,2 @@
+wget https://datasets.aicrowd.com/default/aicrowd-private-datasets/snake-clef-2021/v0.1/training_data.tar.gz
+wget https://aicrowd-production.s3.eu-central-1.amazonaws.com/dataset_files/challenge_3/38c397f7-b983-495f-9bb5-a94d588f9a28_SnakeCLEF2021_train_metadata_PROD.csv?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJ6IZH6GWKDCCDFAQ%2F20211126%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20211126T142446Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=2576024ba080803688503db4f8f1af46fa7a145bc59af8c66b63ffa5234e5481
